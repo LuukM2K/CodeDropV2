3435c7737eb0aa1053cc0611c1d5c5fb3a000ef9
luukm2K <luuk.r.molenbeek@gmail.com>
2019-12-01 21:01:51 +0100

    made sexy

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.10/semantic.min.css" rel="stylesheet"
+          id="bootstrap-css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.10/semantic.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <title>Code Drop</title>
+    <link rel="icon" href="images/codedrop-logo.png">
+</head>
+<body>
+<div id="particles-js">
+    <div class="main-container">
+        <div class="topheader">
+            <span class="announcements">CodeDrop shop, Your all in one Fortnite Exclusive shop</span>
+        </div>
+        <div class="main-header">
+            <div class="main-content">
+                <img class="header-img" src="images/codedrop-logo.png" width="65" height="65">
+                <h2 class="header-title">CodeDrop Shop</h2>
+            </div>
+        </div>
+        <div class="ui container" >
+            <div class="ui four column grid">
+                <div class="row">
+                    <div class="column" style="margin-bottom: 25px;">
+                        <div class="ui card" style="background-color: #1e1e1e">
+                            <div class="image">
+                                <img src="images/eon.png"/>
+                            </div>
+                            <div class="content">
+                                <a class="header" style="color: #e1e1e1">Mini Eon skin + 500 Vbucks</a>
+                                <div class="description" style="color: #e1e1e1">
+                                    €15
+                                </div>
+                            </div>
+                            <div class="extra content">
+                                <button data-shoppy-product="RZzxsNy" class="fluid ui button"
+                                        style="background-color: #FF4181; color: #e1e1e1">Pay
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="ui card" style="background-color: #1e1e1e">
+                            <div class="image">
+                                <img src="images/glow.png"/>
+                            </div>
+                            <div class="content">
+                                <a class="header" style="color: #e1e1e1">Glow Skin + emote</a>
+                                <div class="description" style="color: #e1e1e1">
+                                    €7
+                                </div>
+                            </div>
+                            <div class="extra content">
+                                <button data-shoppy-product="iZzWE9B" class="fluid ui button"
+                                        style="background-color: #FF4181; color: #e1e1e1">Pay
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="ui card" style="background-color: #1e1e1e">
+                            <div class="image">
+                                <img src="images/wonder.png"/>
+                            </div>
+                            <div class="content">
+                                <a class="header" style="color: #e1e1e1">WONDER OUTFIT Code</a>
+                                <div class="description" style="color: #e1e1e1">
+                                    €28
+                                </div>
+                            </div>
+                            <div class="extra content">
+                                <button data-shoppy-product="Kzd1CyR" class="fluid ui button"
+                                        style="background-color: #FF4181; color: #e1e1e1">Pay
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="ui card" style="background-color: #1e1e1e">
+                            <div class="image">
+                                <img src="images/honor.png"/>
+                            </div>
+                            <div class="content">
+                                <a class="header" style="color: #e1e1e1">HONOR OUTFIT Code</a>
+                                <div class="description" style="color: #e1e1e1">
+                                    €20
+                                </div>
+                            </div>
+                            <div class="extra content">
+                                <button data-shoppy-product="1fTq7Sd" class="fluid ui button"
+                                        style="background-color: #FF4181; color: #e1e1e1">Pay
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="ui card" style="background-color: #1e1e1e">
+                            <div class="image">
+                                <img src="images/honor.png"/>
+                            </div>
+                            <div class="content">
+                                <a class="header" style="color: #e1e1e1">HONOR OUTFIT Code</a>
+                                <div class="description" style="color: #e1e1e1">
+                                    €20
+                                </div>
+                            </div>
+                            <div class="extra content">
+                                <button data-shoppy-product="1fTq7Sd" class="fluid ui button"
+                                        style="background-color: #FF4181; color: #e1e1e1">Pay
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="ui card" style="background-color: #1e1e1e">
+                            <div class="image">
+                                <img src="images/honor.png"/>
+                            </div>
+                            <div class="content">
+                                <a class="header" style="color: #e1e1e1">HONOR OUTFIT Code</a>
+                                <div class="description" style="color: #e1e1e1">
+                                    €20
+                                </div>
+                            </div>
+                            <div class="extra content">
+                                <button data-shoppy-product="1fTq7Sd" class="fluid ui button"
+                                        style="background-color: #FF4181; color: #e1e1e1">Pay
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="ui card" style="background-color: #1e1e1e">
+                            <div class="image">
+                                <img src="images/honor.png"/>
+                            </div>
+                            <div class="content">
+                                <a class="header" style="color: #e1e1e1">HONOR OUTFIT Code</a>
+                                <div class="description" style="color: #e1e1e1">
+                                    €20
+                                </div>
+                            </div>
+                            <div class="extra content">
+                                <button data-shoppy-product="1fTq7Sd" class="fluid ui button"
+                                        style="background-color: #FF4181; color: #e1e1e1">Pay
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="ui card" style="background-color: #1e1e1e">
+                            <div class="image">
+                                <img src="images/honor.png"/>
+                            </div>
+                            <div class="content">
+                                <a class="header" style="color: #e1e1e1">HONOR OUTFIT Code</a>
+                                <div class="description" style="color: #e1e1e1">
+                                    €20
+                                </div>
+                            </div>
+                            <div class="extra content">
+                                <button data-shoppy-product="1fTq7Sd" class="fluid ui button"
+                                        style="background-color: #FF4181; color: #e1e1e1">Pay
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
+    <script src="https://shoppy.gg/api/embed.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+
+    <script>
+        particlesJS.load('particles-js', 'assets/particles.json', function () {
+            console.log('callback - particles.js config loaded');
+        });
+
+    </script>
+
+</body>
+</html>
+<?php
