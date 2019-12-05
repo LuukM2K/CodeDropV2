5b6e4acb01187ec79dda4f60c5ee00ef31d55e65
luukm2K <luuk.r.molenbeek@gmail.com>
2019-12-05 23:03:45 +0100

    krayzi

--- faq.php
+++ faq.php
@@ -9,7 +9,9 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.10/semantic.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="css/accordion.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Code Drop</title>
+    <link href="css/dropdown.css" rel="stylesheet">
     <link rel="icon" href="images/codedrop-logo.png">
 </head>
 <body>
@@ -24,7 +26,17 @@
     <div class="main-container">
         <div class="topheader">
             <ul class="menu">
-                <li><a href="index.php">Products</a></li>
+                <div class="dropdown">
+                    <button class="dropbtn">Products
+                        <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-content">
+                        <a href="epic.php">Epic games</a>
+                        <a href="xbox.php">Xbox</a>
+                        <a href="ps4.php">Ps4</a>
+                        <a href="#">Vbucks</a>
+                    </div>
+                </div>
                 <li><a href="faq.php">FAQ</a></li>
                 <li>Socials</li>
                 <li>Contact</li>
@@ -39,7 +51,8 @@
                 <h3>How fast will I get my order?</h3>
                 <div class="content">
                     <div class="content-inner">
-                        <p>Usually, orders will be done within 5-20 minutes. Our maximum delivery time is 2 hours, but usually it will take no longer than 30 minutes.</p>
+                        <p>Usually, orders will be done within 5-20 minutes. Our maximum delivery time is 2 hours, but
+                            usually it will take no longer than 30 minutes.</p>
                     </div>
                 </div>
             </div>
@@ -50,7 +63,8 @@
                 <h3>Are you legit?</h3>
                 <div class="content">
                     <div class="content-inner">
-                        <p>Yes, we are legit. You can search on Twitter #CDSlegit. Also Saft has been selling for multiple months and has over 500 purchases..</p>
+                        <p>Yes, we are legit. You can search on Twitter #CDSlegit. Also Saft has been selling for
+                            multiple months and has over 500 purchases..</p>
                     </div>
                 </div>
             </div>
@@ -61,7 +75,8 @@
                 <h3>What will happen as soon as I paid?</h3>
                 <div class="content">
                     <div class="content-inner">
-                        <p>As soon as you paid, we will see your order and do it for you within 5-20 minutes. If the order wasn't done within 30 minutes, please send me a message on Twitter or Instagram.</p>
+                        <p>As soon as you paid, we will see your order and do it for you within 5-20 minutes. If the
+                            order wasn't done within 30 minutes, please send me a message on Twitter or Instagram.</p>
                     </div>
                 </div>
             </div>
@@ -72,12 +87,14 @@
                 <h3>No Refunds / No Replacements</h3>
                 <div class="content">
                     <div class="content-inner">
-                        <p>No Refunds / No Replacements since all codes are checked before sending out. For products which require account login information we also will not provide any refunds since we apply the product to the account which you give us in order details.</p>
+                        <p>No Refunds / No Replacements since all codes are checked before sending out. For products
+                            which require account login information we also will not provide any refunds since we apply
+                            the product to the account which you give us in order details.</p>
                     </div>
                 </div>
             </div>
         </div>
-<!--        <script src="https://infograph.venngage.com/js/embed/v1/embed.js" data-vg-id="9liFlDkT570" data-title="" data-w="1200" data-h="700" style="opacity: 10%" data-multipage="true"></script>-->
+        <!--        <script src="https://infograph.venngage.com/js/embed/v1/embed.js" data-vg-id="9liFlDkT570" data-title="" data-w="1200" data-h="700" style="opacity: 10%" data-multipage="true"></script>-->
 
     </div>
     <!--<script src="assets/accordion.js" rel="script"></script>-->
